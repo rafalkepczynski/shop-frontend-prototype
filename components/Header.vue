9166d70ff3ad8416b3312ade30268bd21134e189
Lukas Kirylak <contact@lukask.pro>
2020-04-02 16:21:56 +0000

    Add basic navgation

--- components/Header.vue
+++ components/Header.vue
@@ -6,6 +6,14 @@
         <small>Prototyp sklepu z maseczkami i testami na COVID-19</small>
       </h1>
     </div>
+    <nav class="container">
+      <n-link to="/">
+        Strona główna
+      </n-link>
+      <n-link to="/o-nas">
+        O nas
+      </n-link>
+    </nav>
   </header>
 </template>
 
@@ -17,6 +25,34 @@ header {
   small {
     display: block;
     font-size: 0.6em;
+  }
+
+  h1 {
+    margin-bottom: 0.2em;
+  }
+
+  nav {
+    display: flex;
+    flex-direction: row;
+
+    padding: 0;
+
+    a {
+      display: inline-block;
+      padding: 10px 15px;
+      color: inherit;
+      text-decoration: none;
+
+      font-size: 1rem;
+
+      &:hover {
+        background: rgba(0,0,0,0.25);
+      }
+
+      &.nuxt-link-exact-active {
+        background: rgba(0,0,0,0.15);
+      }
+    }
   }
 }
 </style>
--- pages/o-nas.vue
+++ pages/o-nas.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="container">
-    Strona główna
+    O nas
   </div>
 </template>
