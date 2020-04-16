6dd74340cae99ff695ecb3f5e3ba77c9eadb2f97
Lukas Kirylak <contact@lukask.pro>
2020-04-16 11:55:32 +0000

    Add "About us" text

--- pages/o-nas.vue
+++ pages/o-nas.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
-    O nas
+    <h2>O nas</h2>
+    <p>Sklep został założony przez młodych i ambitnych studentów Wojskowej Akademii Technicznej, w trosce o zdrowie i bezpieczeństwo polskich obywateli zareagowaliśmy na szalejącą na świecie pandemię koronawirusa. Zaopatrujemy się u zaufanych chińskich producentów. Sklep powstał w 2020 roku.</p>
   </div>
 </template>
