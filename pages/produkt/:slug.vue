72311b392c5f7c1f8c7747afe2d1dc134b8457ec
randomusername7312 <pawel.gawron1410@gmail.com>
2020-04-30 11:37:27 +0000

    changed produkt slug.vue

--- pages/produkt/:slug.vue
+++ pages/produkt/:slug.vue
@@ -6,7 +6,26 @@
     </template>
     <template v-else>
       <h1>{{ product.name }}</h1>
-      <p>...</p>
+      <div class="image">
+        <img :src="product.image">
+      </div>
+      <div class="test">
+        <div class="productPrice">
+          <h3>
+            <small v-if="product.discountedPrice">
+              <span class="new-price">{{ product.discountedPrice }} zł</span>
+              <span class="old-price">{{ product.price }} zł</span>
+            </small>
+            <small v-else>{{ product.price }} zł</small>
+          </h3>
+        </div>
+        <div class="addProduct">
+          <button>Dodaj do koszyka</button>
+        </div>
+        <div class="productInfo">
+          <p>{{ product.description }}</p>
+        </div>
+      </div>
     </template>
   </div>
 </template>
@@ -27,3 +46,77 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.container{
+  display:block;
+
+  .image{
+    float: left;
+    width: 400px;
+    height: 400px;
+    display: block;
+    margin: 0;
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+
+  .test{
+    display: block;
+    clear: left;
+
+    .productPrice{
+      display: block;
+      float: left;
+      h3 {
+        padding-top: 8px;
+        margin-top: 0;
+
+        small {
+          display: inline-block;
+          margin-bottom: 0.0em;
+          font-size: 2.0em;
+
+          .new-price {
+            color: #4CAF50;
+          }
+
+          .old-price {
+            text-decoration: line-through;
+            font-size: 0.8em;
+          }
+        }
+      }
+    }
+    .addProduct{
+      display: block;
+      position: relative;
+      padding-top: 0px;
+      margin-left: 275px;
+      button{
+        margin-top: 8px;
+        line-height: 40px;
+        font-weight: bold;
+        color: $brand-text;
+        background: $brand-background;
+        border: none;
+      }
+      button:hover{
+        margin-top: 8px;
+        line-height: 40px;
+        font-weight: bold;
+        color: $brand-text;
+        background: rgb(255, 0, 0);
+        border: none;
+      }
+
+    }
+    .productInfo{
+      display: inline-block;
+    }
+  }
+
+}
+
+</style>
